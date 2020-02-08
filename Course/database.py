a5b8a17f7274e61c06693e3c155b11ca868da18f
wolfbolin <wolfbolin@foxmail.com>
2020-02-08 17:27:07 +0800

    【Course】SQL语句更新

--- Course/database.py
+++ Course/database.py
@@ -4,7 +4,7 @@
 # 写入教室信息
 def write_room_info(conn, version, room_data):
     cursor = conn.cursor()
-    sql = "REPLACE INTO `room` (`code`, `name`, `type`, `faculty`, `version`) " \
+    sql = "REPLACE INTO `course` (`code`, `name`, `type`, `faculty`, `version`) " \
           "VALUES (%s, %s, %s, %s, %s)"
     cursor.execute(sql, args=[room_data["code"], room_data["name"], room_data["type"],
                               room_data["faculty"], version])
