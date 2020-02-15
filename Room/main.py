6865e36c24a1e246eda6d4a627f9bb29d75c2875
wolfbolin <wolfbolin@foxmail.com>
2020-02-15 23:03:43 +0800

    【Room】Update

--- Room/main.py
+++ Room/main.py
@@ -24,7 +24,8 @@ def main(config, semester, version):
     active_list = Common.fetch_active_list(config, version, "可用教室", "act_room", "jslb", semester)
 
     # 更新教室课表
-    Common.fetch_class_table(_config, "2019-11-27", "课表教室", "room_table", "jskb", "2019-2020-1", active_list)
+    Common.fetch_class_table(_config, version, "课表教室", "room_table", "jskb", "2019-2020-1", active_list)
+    Common.merge_table_info(_config, version, "课表教室", "room_table", "room")
 
 
 if __name__ == "__main__":
